55257df9b3ba68de8bc2dd54f8a48c7a4582884a
AhmedMuktadir14218 <ahmed15-14218@diu.edu.bd>
2023-05-18 14:55:41 +0600

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const cors = require('cors');
-
+const port = process.env.PORT || 5000;
 const app = express();
-
+//middleWare
 app.use(cors())
-app
+app.use(express.json());
+
+app.get('/',async(req,res) =>{
+    res.send('gentlemans server is running');
+})
+
+app.listen(port,() => 
+    console.log(`gentlemans server is running ${5000}`)
+)
