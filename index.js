cdce3c26c90262206bc8bf01872f4c139e65f7a1
AhmedMuktadir14218 <ahmed15-14218@diu.edu.bd>
2023-05-18 20:41:11 +0600

    first commit

--- index.js
+++ index.js
@@ -19,27 +19,60 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jgfdfab.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   }
-// });
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
-    // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
+    console.log('db connected');
+   const appointmentOptionCollection = client.db('gentlemans-cut').collection('appointmentOptions');
+   const bookingnsCollection = client.db('gentlemans-cut').collection('bookings');
+   const bookingListCollection = client.db('gentlemans-cut').collection('bookings');
+
+  //  const appointmentOptionCollection = client.db('doctors-chamber').collection('service');
+
+
+
+   app.get('/appointmentOptions', async(req, res) => {
+    const date =req.query.date;
+    console.log(date);
+    const query={};
+    const options= await appointmentOptionCollection.find(query).toArray();
+    res.send(options)
+  })  
+
+
+
+   app.post('/bookings', async(req, res) => {
+      
+    const booking=req.body;
+    console.log(booking);
+    
+    const result= await bookingnsCollection.insertOne(booking);
+    res.send(result);
+  })
+
+
+  app.get('/bookinglist', async(req, res) => {
+     const query={};
+    const options= await bookingListCollection.find(query).toArray();
+    res.send(options)
+  })  
+    
+  } 
+  
+  
+  finally {
+
   }
 }
 run().catch(console.dir);
+
 
 
 
@@ -50,3 +83,8 @@ app.get('/',async(req,res) =>{
 app.listen(port,() => 
     console.log(`gentlemans server is running ${5000}`)
 )
+
+
+
+
+// console.log("Pinged your deployment. You successfully connected to MongoDB!");
