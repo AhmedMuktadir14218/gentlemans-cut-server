5eb0fc6a788d001d570477f3ebbce7d0b1d9c0d7
AhmedMuktadirUdoy <100287308+AhmedMuktadir14218@users.noreply.github.com>
2023-06-07 01:06:29 +0600

    Delete .history directory

--- .history/index_20230518141445.js
+++
@@ -1,4 +0,0 @@
-const express = require('express');
-const cors = require('cors');
-
-const app = express();
--- .history/index_20230518141505.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const cors = require('cors');
-
-const app = express();
-
-app.use(cors())
-app
